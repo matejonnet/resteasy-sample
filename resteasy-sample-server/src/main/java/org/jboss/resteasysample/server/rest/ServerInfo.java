f2a42b3382a3213109617eb99a4a668be1ba5274
Matej Lazar <matejonnet@gmail.com>
2012-06-20 15:16:24 +0200

    Removed server-side contet type specfication.

--- resteasy-sample-server/src/main/java/org/jboss/resteasysample/server/rest/ServerInfo.java
+++ resteasy-sample-server/src/main/java/org/jboss/resteasysample/server/rest/ServerInfo.java
@@ -2,7 +2,6 @@ package org.jboss.resteasysample.server.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 
 /**
  * @author Matej Lazar
@@ -12,7 +11,6 @@ public class ServerInfo {
 
     @GET
     @Path("/timeStamp")
-    @Produces({"application/json"})
     public long getTimeStamp() {
         return System.currentTimeMillis();
     }
